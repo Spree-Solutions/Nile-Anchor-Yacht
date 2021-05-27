8d61e15ccc5413aa78e49a69f66568f2cf413612
Farida ElOraby <faridaeloraby@gmail.com>
2021-05-27 17:13:59 +0200

    Added NavBar Style

---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+export default function NavBar() {
+  return <StyledDiv>Trying Font</StyledDiv>;
+}
+const StyledDiv = styled.div`
+  /* box width and shadow */
+  width: 100vw;
+  padding: 1.1vw 0.7vw 1.45vw 0.7vw;
+  font-family: Askan !important;
+`;
