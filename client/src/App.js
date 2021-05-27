8ca65bebe7ad4fc373000f83639e0073da61419a
Farida ElOraby <faridaeloraby@gmail.com>
2021-05-27 22:53:53 +0200

    Added Fonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return <div className="App"></div>;
--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 export default function NavBar() {
-  return <StyledDiv>Trying Font</StyledDiv>;
+  return <StyledDiv>Who Are We?</StyledDiv>;
 }
 const StyledDiv = styled.div`
   /* box width and shadow */
   width: 100vw;
   padding: 1.1vw 0.7vw 1.45vw 0.7vw;
-  font-family: Askan !important;
+  font-family: "Askan Regular" !important;
 `;
