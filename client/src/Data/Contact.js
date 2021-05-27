ed48a6b4168bf04e779930ffc3566548e97421fb
Farida ElOraby <faridaeloraby@gmail.com>
2021-05-27 16:11:10 +0200

    Added English Static Data

---
+++ client/src/Data/Contact.js
@@ -0,0 +1,14 @@
+const Title = "Book Now";
+const ButtonLabel = ["Request a", "Register to our", "Chat with us on"];
+const ButtonAction = ["Free Call-back", "Newsletter", "Whatsapp"];
+const Form = ["Your Name", "Time", "Your Email", "Event Type"];
+const CheckBox = "Send me newsletter";
+const Button = "Submit";
+module.exports = {
+  Title,
+  ButtonLabel,
+  ButtonAction,
+  Form,
+  CheckBox,
+  Button,
+};
---
+++ client/src/Data/WhoAreWe.js
@@ -0,0 +1,12 @@
+const Title = "Welcome";
+const TitleBold = "On Board";
+const SubTitle = "Who Are We";
+const body =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus at justo, tincidunt molestie enim lacus nibh pharetra. Nisi, mauris eget pellentesque mattis habitant enim vel, sed id. Vel mauris nibh nullam faucibus. Eget tellus vel arcu venenatis ligula volutpat elementum ut justo. Neque mauris ullamcorper purus vitae mauris, dolor scelerisque phasellus. Nisl, ipsum sed est in pulvinar. Enim ac non aliquet eget ornare.";
+
+module.exports = {
+  Title,
+  TitleBold,
+  SubTitle,
+  body,
+};
