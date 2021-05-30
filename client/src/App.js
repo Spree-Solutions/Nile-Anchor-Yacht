62780486940157a26c6403879217bc6322ec1ae1
FaridaElOraby <41236592+FaridaElOraby@users.noreply.github.com>
2021-05-30 21:37:28 +0200

    Added Integration of Page & References for Scrolling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,44 @@
 import "./App.css";
-import NavBar from "./Components/Footer";
+import { useRef } from "react";
+import Welcome from "./Components/Welcome";
+import OurYachts from "./Components/OurYachts";
+import Gallery from "./Components/Gallery";
+import OurServices from "./Components/OurServices";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
+  const References = {
+    WelceomeRef: useRef(null),
+    OurYachtsRef: useRef(null),
+    GalleryRef: useRef(null),
+    OurServicesRef: useRef(null),
+    ContactRef: useRef(null),
+  };
+
+  function executeScroll(key) {
+    key.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+
   return (
     <div className="App">
-      <NavBar />
+      <div ref={References.WelceomeRef}>
+        <Welcome executeScroll={executeScroll} References={References} />
+      </div>
+
+      <div ref={References.OurYachtsRef}>
+        <OurYachts />
+      </div>
+      <div ref={References.GalleryRef}>
+        <Gallery />
+      </div>
+      <div ref={References.OurServicesRef}>
+        <OurServices />
+      </div>
+      <div ref={References.ContactRef}>
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
 }
