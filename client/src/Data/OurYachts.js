d3f85110395b40bb51853357919f4e8bef3bd1f1
FaridaElOraby <41236592+FaridaElOraby@users.noreply.github.com>
2021-06-01 00:17:44 +0200

    Updated Our Yachts Section Dummy Data

--- client/src/Data/OurYachts.js
+++ client/src/Data/OurYachts.js
@@ -2,7 +2,7 @@ const Title = "Our";
 const TitleBold = "Yachts";
 const List = ["Gallery", "Book Now"];
 const body =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit nisi eget aliquam cras consequat ac nunc pretium. Dictumst felis neque ullamcorper blandit. In condimentum arcu scelerisque ligula posuere. Ac scelerisque id.";
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit nisi eget aliquam cras consequat ac nunc pretium. Dictumst felis neque ullamcorper.";
 const bodyBold = "350 Passengers";
 
 module.exports = {
