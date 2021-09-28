9150a1a103a516f192c9a921dd0e9436cc4a4a1e
FaridaElOraby <41236592+FaridaElOraby@users.noreply.github.com>
2021-09-28 19:05:03 +0200

    Updated Theme

--- client/src/Styles/Colors.js
+++ client/src/Styles/Colors.js
@@ -1,7 +1,7 @@
 //all hex of colors from design assets
 
 const colors = {
-  Black: "#433D3D",
+  Black: "#000000",
   White: "#FFFFFF",
   Merino: "#E6DBCE",
   Merino2: "#E8E1D7",
@@ -11,6 +11,9 @@ const colors = {
   DarkBlack: "#161616",
   DarkGrey2: "#4D4D4F",
   MainBeige: "#E6E1D6",
+  Navy: "#183F5E",
+  DarkBrown: "#251C15",
+  Pink: "#A7564A",
 };
 
 module.exports = {
--- client/src/Styles/Selector.js
+++ client/src/Styles/Selector.js
@@ -7,7 +7,7 @@ export default function Selector(props) {
     <StyledDiv>
       <select
         onChange={(e) => props.setSelected(e.target.value)}
-        className="Selector"
+        className={props.language === "EN" ? "SelectorEN" : "SelectorAR"}
       >
         {props.list.map((item) => (
           <option className="Option" value={item}>
@@ -21,9 +21,9 @@ export default function Selector(props) {
 
 const StyledDiv = styled.div`
   margin-bottom: 1.4vw;
-  .Selector,
-  .Selector:hover,
-  .Selector:active {
+  .SelectorEN,
+  .SelectorEN:hover,
+  .SelectorEN:active {
     width: 21.4vw;
     text-align-last: left;
     padding: 0.4vw 0vw 0.4vw 0.6vw;
@@ -52,13 +52,55 @@ const StyledDiv = styled.div`
   }
 
   @media screen and (max-width: 620px) {
-    .Selector,
-    .Selector:hover,
-    .Selector:active {
+    .SelectorEN,
+    .SelectorEN:hover,
+    .SelectorEN:active {
       background: ${colors.Merino}
         url('data:image/svg+xml;utf8,<svg width="8" height="4" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><g><path d="M0 0H15.5L8 8L0 0Z" fill="%234D4D4F"/></g></svg>')
         no-repeat;
       background-position-x: 17vw !important;
+      background-position-y: 50% !important;
+    }
+  }
+
+  .SelectorAR,
+  .SelectorAR:hover,
+  .SelectorAR:active {
+    width: 21.4vw;
+    text-align-last: right;
+    padding: 0.4vw 0.6vw 0.4vw 0vw;
+    font-family: "Askan Light" !important;
+    border-color: ${colors.Merino}!important;
+    outline: none;
+    font-size: 1vw;
+    color: ${colors.Black};
+    cursor: pointer;
+
+    /* adding new arrow    */
+
+    background: ${colors.Merino}
+      url('data:image/svg+xml;utf8,<svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><g><path d="M0 0H15.5L8 8L0 0Z" fill="%234D4D4F"/></g></svg>')
+      no-repeat;
+    background-position-x: 1vw !important;
+    background-position-y: 50% !important;
+
+    /* remove preexisiting arrow  */
+
+    -webkit-appearance: none !important;
+    -moz-appearance: none !important;
+    -ms-appearance: none !important;
+    -o-appearance: none !important;
+    appearance: none !important;
+  }
+
+  @media screen and (max-width: 620px) {
+    .SelectorAR,
+    .SelectorAR:hover,
+    .SelectorAR:active {
+      background: ${colors.Merino}
+        url('data:image/svg+xml;utf8,<svg width="8" height="4" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><g><path d="M0 0H15.5L8 8L0 0Z" fill="%234D4D4F"/></g></svg>')
+        no-repeat;
+      background-position-x: 0.9vw !important;
       background-position-y: 50% !important;
     }
   }
