36231203c753c4c129fa6b32f1d099463516b395
tanawys <amraeltanawy@gmail.com>
2021-10-01 16:58:49 +0200

    Reservation handler initial definition

---
+++ client/src/Utilities/ReservationHandler.js
@@ -0,0 +1,24 @@
+
+
+
+
+export default class ReservationHandler {
+    username="";
+    phoneNumber="";
+    email="";
+    selectedYacht=null;
+    _selectedReservationDay=null;
+    selectedStartingTime=null;
+    selectedEndingTime=null;
+    calculatedFinalPrice=0;
+    isLoading=false;
+    availableYachts=[];
+
+
+    // get yacht availability given date
+    // reserve yacht on a given date
+    // calculate yacht price for a given time period same day
+
+    
+
+}
