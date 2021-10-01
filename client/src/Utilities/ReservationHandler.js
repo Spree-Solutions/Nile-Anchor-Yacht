2e200d8a6aaf8ada02369dcd9a69210bf546d3fb
tanawys <amraeltanawy@gmail.com>
2021-10-01 21:45:36 +0200

    initial availability inversion logic

--- client/src/Utilities/ReservationHandler.js
+++ client/src/Utilities/ReservationHandler.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 export default class ReservationHandler {
     username="";
     phoneNumber="";
@@ -15,34 +11,52 @@ export default class ReservationHandler {
     isLoading=false;
     availableYachts=[];
     priceSegments=[];
+    availableHourSegments=[]; // type []{startTime:"hh:mm",endTime:"hh:mm"}
+    existingReservations = [];
 
 
     // get yacht availability given date
     // reserve yacht on a given date
     // calculate yacht price for a given time period same day
 
-calculatePrice(hoursCount){
-    // check if type is valid
-    // if(typeof(hoursCount)===typeof(0))
+    calculatePrice(hoursCount){
+        // check if type is valid
+        // if(typeof(hoursCount)===typeof(0))
 
-    // sort by from hours smallest first
-    let sortedPriceList = this.priceSegments.sort(((hoursA,hoursB)=> Number(hoursA.fromHours)- Number(hoursB.fromHours)));
-    let accumulator =0;
-    let lastInterval;
-    while(hoursCount>0){
-        if(sortedPriceList.length>0){
-            lastInterval = sortedPriceList.shift();
+        // sort by from hours smallest first
+        let sortedPriceList = this.priceSegments.sort(((hoursA,hoursB)=> Number(hoursA.fromHours)- Number(hoursB.fromHours)));
+        let accumulator =0;
+        let lastInterval;
+        while(hoursCount>0){
+            if(sortedPriceList.length>0){
+                lastInterval = sortedPriceList.shift();
+            }
+            if(hoursCount<1){
+                // hours fractions
+                accumulator += (Number(lastInterval.price)*hoursCount);
+            } else {
+                
+                accumulator += Number(lastInterval.price);
+            }
         }
-        if(hoursCount<1){
-            // hours fractions
-            accumulator += (Number(lastInterval.price)*hoursCount);
-        } else {
-            
-            accumulator += Number(lastInterval.price);
-        }
+        return accumulator;
     }
-    return accumulator;
-}
+
+    calculateAvailablity(){
+        let hoursArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+        // sort reservation earlier first
+        reservations = this.existingReservations.sort(((hoursA,hoursB)=> Number(hoursA.fromHours)- Number(hoursB.fromHours)))
+
+        let connectedSegments = reservations.map(reservation=>{
+            let startHour = hoursArray.indexOf(Math.floor(reservation.fromHours));
+            let endingHour = hoursArray.indexOf(Math.ceil(reservation.toHours));
+            let singleSegment = {fromHours:hoursArray[0],toHours:hoursArray[startHour-1]};
+            hoursArray = hoursArray.splice(startHour,endingHour-startHour);
+            return singleSegment
+        });
+
+        return connectedSegments;
+    } 
 
 
     
