d963d454ba97fe91bf60eff94e993f6f6d55b97b
tanawys <amraeltanawy@gmail.com>
2021-10-04 16:30:11 +0200

    add day picker styling

--- client/src/NewComponents/DatePicker/DayDatePicker.js
+++ client/src/NewComponents/DatePicker/DayDatePicker.js
@@ -1,16 +1,49 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-
+import {colors } from "../../Styles/Colors";
+import {createGlobalStyle} from 'styled-components';
 import "react-datepicker/dist/react-datepicker.css";
+
+const DatePickerWrapperStyles = createGlobalStyle`
+margin-bottom: 1.4vw;
+.DatePicker,
+.DatePicker:hover,
+.DatePicker:active {
+  text-align-last: left;
+  padding: 0.4vw 0.6vw 0.4vw 0.6vw;
+  font-family: "Askan Light" !important;
+  border: none;
+  outline: none;
+  font-size: 1vw;
+  color: ${colors.Black};
+  cursor: pointer;
+  background-color: ${colors.Merino} !important;
+}
+.fullWidth {
+    width: 100%;
+}
+.hideOverflow {
+    overflow: hidden;
+}
+.ENDatePicker {
+  margin-right: 1.6vw;
+}
+
+.ARDatePicker {
+  margin-left: 1.6vw;
+}
+`;
 
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 export const DatePickerComponent = (props) => {
 
-    let initialValue = new Date();
+    let initialValue =null;
     if (typeof props.initialValue === typeof initialValue){
         initialValue = props.initialValue;
+    } else {
+        initialValue = null;
     }
     const [startDate, setStartDate] = useState(initialValue);
 
@@ -24,6 +57,10 @@ export const DatePickerComponent = (props) => {
     }
 
     return (
-        <DatePicker selected={startDate} onChange={onChangeHandler} />
+        <React.Fragment>
+
+            <DatePicker wrapperClassName="hideOverflow" className="DatePicker fullWidth" value={!startDate?"Select a date":undefined} selected={startDate? startDate:undefined} onChange={onChangeHandler} />
+            <DatePickerWrapperStyles />
+        </React.Fragment>
     );
 };
