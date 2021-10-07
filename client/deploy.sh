d98b0816d1a41960dcc752215fee3451a068cb3c
hassan-kandil <hassanosama@aucegypt.edu>
2021-10-07 15:10:49 +0200

    Updated ecr url in deploy.sh

--- client/deploy.sh
+++ client/deploy.sh
@@ -5,9 +5,9 @@ $(aws ecr get-login --no-include-email --region us-east-2 --profile=nileanchor);
 
 export IMAGE_TAG=$1;
 
-docker pull 355666785320.dkr.ecr.us-east-2.amazonaws.com/nileanchor:$IMAGE_TAG;
+docker pull 397937426503.dkr.ecr.us-east-2.amazonaws.com/nileanchor:$IMAGE_TAG;
 
 docker stack deploy -c docker-compose.prod.yml --with-registry-auth nileanchor;
 
-docker logout https://355666785320.dkr.ecr.us-east-2.amazonaws.com \
+docker logout https://397937426503.dkr.ecr.us-east-2.amazonaws.com \
   && echo "Deployment Done 🚀";
