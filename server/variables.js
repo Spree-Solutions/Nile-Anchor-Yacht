72ac3003d661c835ad52121639f0c20f5e99bc46
hassan-kandil <hassanosama@aucegypt.edu>
2021-10-09 17:25:50 +0200

    Fixed a typo with configs urls.

--- server/variables.js
+++ server/variables.js
@@ -1,8 +1,8 @@
 const configs = {
     BACKEND_API_BASE_URL:"http://nile-anchor.ddns.net:8080/erp",
-    BACKEND_API_ALIAS:"http://localhost:3001/api2",
+    BACKEND_API_ALIAS:"http://thenileanchor.com/api2",
     COWPAY_BASE_URL:"https://staging.cowpay.me/api/v1",
-    COWPAY_API_ALIAS:"http://localhost:3001/api1"
+    COWPAY_API_ALIAS:"http://thenileanchor.com/api1"
 }
 
 module.exports = configs;
