1a6412d585d4e5ee4bd59981286adda3d8b04917
hassan-kandil <hassanosama@aucegypt.edu>
2021-10-09 16:41:41 +0200

    Updated deployment env to include proxy server.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ $(aws ecr get-login --no-include-email --region us-east-2 --profile=nileanchor);
 export IMAGE_TAG=$1;
 
 docker pull 397937426503.dkr.ecr.us-east-2.amazonaws.com/nileanchor:$IMAGE_TAG;
+docker pull 397937426503.dkr.ecr.us-east-2.amazonaws.com/nileanchor/proxy:$IMAGE_TAG;
 
 docker stack deploy -c docker-compose.prod.yml --with-registry-auth nileanchor;
 
