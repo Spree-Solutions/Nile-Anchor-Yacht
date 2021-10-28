c9a0d35c41832b48ce9bc1d83f93035adb80571d
Khaled Ayman <khaledaymanmoh@gmail.com>
2021-10-28 12:56:38 +0200

    Adds our yacht new design

--- client/src/NewComponents/HomePage/OurYachts.js
+++ client/src/NewComponents/HomePage/OurYachts.js
@@ -6,6 +6,7 @@ import { colors } from "../../Styles/Colors";
 import Liberty_Light from "../../Images/Liberty_Light.png";
 import Liberty_Image from "../../Images/Liberty_Image.png";
 import BellaYellow from "../../Images/BellaYellow.png";
+import OurYachtsNewDesign from "./OurYachtsNewDesign";
 
 export default function OurYachts(props) {
   const Data = props.language === "EN" ? DataEnglish : DataArabic;
@@ -131,126 +132,127 @@ export default function OurYachts(props) {
           </div>
         </div>
       ) : (
-        <div
-          className={
-            props.language === "EN" ? "LeftTextAlign" : "RightTextAlign"
-          }
-        >
-          <table>
-            <tbody>
-              <tr>
-                <td className="Col1 Title">
-                  <div className="Title"> {Data.TitleBold}</div>
-                  <div className="TitleRegular"> {Data.Title}</div>
-                  {Data.List.map((item, index) => (
-                    <div className="ListItem">
-                      <svg
-                        width="0.7vw"
-                        height="0.7vw"
-                        viewBox="0 0 0.7vw 0.7vw"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className={
-                          props.language === "EN" ? "ENbullet" : "ARbullet"
-                        }
-                      >
-                        <rect
-                          width="0.7vw"
-                          height="0.7vw"
-                          fill={colors.Black}
-                        />
-                      </svg>{" "}
-                      <a href={`${index === 0 ? "/#gallery" : "/#booking"}`}>
-                        {item}
-                      </a>
-                    </div>
-                  ))}
-                </td>
-                <td>
-                  <table>
-                    <tbody>
-                      <tr>
-                        <td>
-                          {" "}
-                          <div className="Col2">
-                            <img
-                              src={Liberty_Image}
-                              alt="Liberty"
-                              className="Liberty_Image"
-                            />
-                          </div>
-                        </td>
-                        <td className="Col3">
-                          <div className="buttons">
-                            {Data.Options.map((yacht, index) => {
-                              if (index !== 0) {
-                                return (
-                                  <span>
-                                    {" | "}
-                                    <span
-                                      onClick={() => setChosenYacht(yacht)}
-                                      className={
-                                        chosenYacht === yacht
-                                          ? "chosenButton"
-                                          : "clickable"
-                                      }
-                                    >
-                                      {yacht}
-                                    </span>
-                                  </span>
-                                );
-                              } else {
-                                return (
-                                  <span
-                                    onClick={() => setChosenYacht(yacht)}
-                                    className={
-                                      chosenYacht === yacht
-                                        ? "chosenButton"
-                                        : "clickable"
-                                    }
-                                  >
-                                    {yacht}
-                                  </span>
-                                );
-                              }
-                            })}
-                          </div>
-                          <div
-                            className={
-                              chosenYacht === "Liberty"
-                                ? "Liberty_Light_Background"
-                                : "Bella_Light_Background"
-                            }
-                          >
-                            {chosenYacht === "Liberty" ? (
-                              <img
-                                src={Liberty_Light}
-                                alt="Liberty"
-                                className="Liberty_Light"
-                              />
-                            ) : (
-                              <img
-                                src={BellaYellow}
-                                alt="Bella"
-                                className="BellaYellow"
-                              />
-                            )}
-                          </div>
-                          <div className="body">
-                            {chosenYacht === "Liberty"
-                              ? Data.LibertyBody
-                              : Data.BellaBody}
-                          </div>
-                          {/* <div className="bodyBold">{Data.ReadMore}</div> */}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <OurYachtsNewDesign />
+        // <div
+        //   className={
+        //     props.language === "EN" ? "LeftTextAlign" : "RightTextAlign"
+        //   }
+        // >
+        //   <table>
+        //     <tbody>
+        //       <tr>
+        //         <td className="Col1 Title">
+        //           <div className="Title"> {Data.TitleBold}</div>
+        //           <div className="TitleRegular"> {Data.Title}</div>
+        //           {Data.List.map((item, index) => (
+        //             <div className="ListItem">
+        //               <svg
+        //                 width="0.7vw"
+        //                 height="0.7vw"
+        //                 viewBox="0 0 0.7vw 0.7vw"
+        //                 fill="none"
+        //                 xmlns="http://www.w3.org/2000/svg"
+        //                 className={
+        //                   props.language === "EN" ? "ENbullet" : "ARbullet"
+        //                 }
+        //               >
+        //                 <rect
+        //                   width="0.7vw"
+        //                   height="0.7vw"
+        //                   fill={colors.Black}
+        //                 />
+        //               </svg>{" "}
+        //               <a href={`${index === 0 ? "/#gallery" : "/#booking"}`}>
+        //                 {item}
+        //               </a>
+        //             </div>
+        //           ))}
+        //         </td>
+        //         <td>
+        //           <table>
+        //             <tbody>
+        //               <tr>
+        //                 <td>
+        //                   {" "}
+        //                   <div className="Col2">
+        //                     <img
+        //                       src={Liberty_Image}
+        //                       alt="Liberty"
+        //                       className="Liberty_Image"
+        //                     />
+        //                   </div>
+        //                 </td>
+        //                 <td className="Col3">
+        //                   <div className="buttons">
+        //                     {Data.Options.map((yacht, index) => {
+        //                       if (index !== 0) {
+        //                         return (
+        //                           <span>
+        //                             {" | "}
+        //                             <span
+        //                               onClick={() => setChosenYacht(yacht)}
+        //                               className={
+        //                                 chosenYacht === yacht
+        //                                   ? "chosenButton"
+        //                                   : "clickable"
+        //                               }
+        //                             >
+        //                               {yacht}
+        //                             </span>
+        //                           </span>
+        //                         );
+        //                       } else {
+        //                         return (
+        //                           <span
+        //                             onClick={() => setChosenYacht(yacht)}
+        //                             className={
+        //                               chosenYacht === yacht
+        //                                 ? "chosenButton"
+        //                                 : "clickable"
+        //                             }
+        //                           >
+        //                             {yacht}
+        //                           </span>
+        //                         );
+        //                       }
+        //                     })}
+        //                   </div>
+        //                   <div
+        //                     className={
+        //                       chosenYacht === "Liberty"
+        //                         ? "Liberty_Light_Background"
+        //                         : "Bella_Light_Background"
+        //                     }
+        //                   >
+        //                     {chosenYacht === "Liberty" ? (
+        //                       <img
+        //                         src={Liberty_Light}
+        //                         alt="Liberty"
+        //                         className="Liberty_Light"
+        //                       />
+        //                     ) : (
+        //                       <img
+        //                         src={BellaYellow}
+        //                         alt="Bella"
+        //                         className="BellaYellow"
+        //                       />
+        //                     )}
+        //                   </div>
+        //                   <div className="body">
+        //                     {chosenYacht === "Liberty"
+        //                       ? Data.LibertyBody
+        //                       : Data.BellaBody}
+        //                   </div>
+        //                   {/* <div className="bodyBold">{Data.ReadMore}</div> */}
+        //                 </td>
+        //               </tr>
+        //             </tbody>
+        //           </table>
+        //         </td>
+        //       </tr>
+        //     </tbody>
+        //   </table>
+        // </div>
       )}
     </StyledDiv>
   );
---
+++ client/src/NewComponents/HomePage/OurYachtsNewDesign.jsx
@@ -0,0 +1,156 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Liberty_Image from "../../Images/Liberty_Image.png";
+import { colors } from "../../Styles/Colors";
+import BellaYellow from "../../Images/BellaYellow.png";
+import Liberty_Dark from "../../Images/libDark.png";
+
+const OurYachtsNewDesign = () => {
+  const [selectedYacht, setSelectedYacht] = useState("liberty");
+  return (
+    <StyledSection>
+      <div className="column column-1">
+        <div className="title">
+          <h1>Yacht</h1>
+          <h1>Collection</h1>
+        </div>
+        <img src={Liberty_Image} alt="" />
+        <a href="/#booking">- Book Now</a>
+      </div>
+      <div className="column column-2">
+        <div className="heading">
+          <p
+            onClick={() => setSelectedYacht("liberty")}
+            className={`${selectedYacht === "liberty" ? "selected" : ""}`}
+          >
+            LIBERTY
+          </p>
+          <p>|</p>
+          <p
+            onClick={() => setSelectedYacht("bella")}
+            className={`${selectedYacht === "bella" ? "selected" : ""}`}
+          >
+            BELLA
+          </p>
+          <div className="separator"></div>
+          <img
+            src={selectedYacht === "bella" ? BellaYellow : Liberty_Dark}
+            alt="logo"
+          />
+        </div>
+        <div className="text">
+          {selectedYacht === "liberty" ? (
+            <p>
+              Liberty - Liberating one’s self to a moment of happiness. The
+              two-story yacht is designed to comfortably accommodate guests who
+              seek to host large; events, celebrations, marriage ceremonies,
+              wedding parties, corporate outings and private meetings on Cairo’s
+              Nile River. The yacht’s exterior and interior design is
+              exquisitely designed to give our delightful guests the feeling of
+              luxury, with a touch of class following a sensible feeling of
+              “where Europe meets the Nile”.
+            </p>
+          ) : (
+            <p>
+              Bella – Italian for beauty, an inner reflection of the yacht’s
+              overall look. Smaller than the liberty yacht in size yet
+              stands-out elegantly as it cruises throughout Cairo’s Nile River.
+              The yacht offers three decks with a classic high- end interior and
+              exterior design with the capacity to cater around 40 passengers.
+              The upper deck comes with tinted windows and a 360-degree
+              panoramic view, giving our delightful guests the chance to
+              privately experience Cairo’s Nile River.
+            </p>
+          )}
+        </div>
+      </div>
+    </StyledSection>
+  );
+};
+const StyledSection = styled.div`
+  display: flex;
+  color: ${colors.Navy};
+  font-family: "Lato Regular";
+  width: 100%;
+  justify-content: space-between;
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .column {
+    display: flex;
+    flex-direction: column;
+  }
+  .column-1 {
+    width: 50%;
+    .title {
+      font-family: "Askan Light";
+      font-size: 2vw;
+      line-height: 3.5vw;
+      h1:first-child {
+        font-weight: 700;
+        font-family: "Askan Bold";
+      }
+      text-align: left;
+      color: ${colors.DarkGrey2};
+    }
+    img {
+      width: 30vw;
+      width: 90%;
+      margin-top: 4vw;
+    }
+    a {
+      text-decoration: none;
+      text-align: left;
+      margin-top: 2vw;
+      color: ${colors.Navy};
+      font-size: 2vw;
+    }
+  }
+  .column-2 {
+    width: 50%;
+    /* justify-content: center; */
+
+    .heading {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      margin-top: 3vw;
+      p {
+        font-size: 2vw;
+        &:not(:first-of-type) {
+          margin-left: 1vw;
+        }
+        &:not(:nth-of-type(2)) {
+          cursor: pointer;
+          text-decoration: underline;
+        }
+        &.selected {
+          font-weight: 600;
+          text-decoration: none;
+        }
+      }
+      .separator {
+        background: ${colors.MainBeige};
+        height: 6vw;
+        width: 0.5vw;
+        margin-left: 2vw;
+      }
+    }
+    img {
+      width: 19vw;
+      height: 6.5vw;
+      object-fit: cover;
+      margin-left: 2vw;
+    }
+    .text {
+      margin-top: 5vw;
+      font-size: 1.5vw;
+      font-weight: 600;
+      text-align: left;
+    }
+  }
+`;
+export default OurYachtsNewDesign;
