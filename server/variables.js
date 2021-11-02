91279404a2744fe35042d744585c6c5d9aaf85dc
tanawys <amraeltanawy@gmail.com>
2021-11-02 19:13:41 +0200

    fix payment error wrong server variables

--- server/variables.js
+++ server/variables.js
@@ -1,7 +1,7 @@
 const configs = {
     BACKEND_API_BASE_URL:"http://nile-anchor.ddns.net:8080/erp",
     BACKEND_API_ALIAS:"http://thenileanchor.com/api2",
-    COWPAY_BASE_URL:"https://staging.cowpay.me/api/v1",
+    COWPAY_BASE_URL:"https://cowpay.me/api/v1",
     COWPAY_API_ALIAS:"http://thenileanchor.com/api1"
 }
 
