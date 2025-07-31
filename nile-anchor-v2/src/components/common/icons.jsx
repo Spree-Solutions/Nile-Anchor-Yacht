940956b9ae7ef80e77ea9356f783533e2d11108c
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-07-31 13:41:55 +0300

    App Icons

---
+++ nile-anchor-v2/src/components/common/icons.jsx
@@ -0,0 +1,9 @@
+import Logo from "../../assets/icons/app-logo.svg?react";
+import ColumnLogo from "../../assets/icons/column_logo.svg?react";
+import EgyptIcon from "../../assets/icons/egypt.svg?react";
+import USIcon from "../../assets/icons/us.svg?react";
+import AnchorIcon from "../../assets/icons/anchor.svg?react";
+import YachtIcon from "../../assets/icons/yacht.svg?react";
+import WheelIcon from "../../assets/icons/wheel.svg?react";
+
+export { Logo, ColumnLogo, AnchorIcon, USIcon, EgyptIcon, YachtIcon, WheelIcon };
