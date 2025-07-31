38860ce919ecb8a8e6e75862d4b2205fc35088c9
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-07-31 13:45:17 +0300

    Add Header and Footer To App Layout

--- nile-anchor-v2/src/layout/AppLayout.jsx
+++ nile-anchor-v2/src/layout/AppLayout.jsx
@@ -1,9 +1,13 @@
+import Footer from "@/components/common/Footer/Footer";
+import Header from "@/components/common/Header/Header";
 import { Outlet } from "react-router-dom";
 
 function AppLayout() {
   return (
     <main>
+      <Header />
       <Outlet />
+      <Footer />
     </main>
   );
 }
