b3b9968a1901ea7fe13a4b0b3579e3c6a6d6cc0a
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-07-31 13:43:47 +0300

    Add App Structure

--- nile-anchor-v2/src/App.jsx
+++ nile-anchor-v2/src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
@@ -6,6 +8,7 @@ import { AppRoutes } from "./routes/AppRoutes";
 import { useLanguage } from "./hooks/helpers/useLanguage";
 import { queryClient } from "./config/queryClient";
 import { DirectionProvider } from "@radix-ui/react-direction";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 function App() {
   const { language } = useLanguage();
@@ -14,6 +17,7 @@ function App() {
     <DirectionProvider dir={language === "en" ? "ltr" : "rtl"}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter future={{ v7_startTransition: true }}>
+          <ScrollToTop />
           <AppRoutes />
         </BrowserRouter>
         <Toaster position="top-right" />
